6f78fbed4d3a7b7de947691005e8de2993d69a3e
MarcusAnglin <21136754+MarcusAnglin@users.noreply.github.com>
2022-06-16 03:47:07 -0400

    Fixed javascript broken by the previous commit

--- sctipts/index.js
+++ sctipts/index.js
@@ -1,8 +1,12 @@
-const ratings = document.querySelectorAll(".number-rating");
-const submitBtn = document.querySelector(".submit-btn");
-const rateContainer = document.querySelector(".prompt-container");
-const thankYouContainer = document.querySelector(".thank-you-container");
+const ratingsContainer = document.querySelector(".rating-container");
+const promptContainer = document.querySelector(".prompt-container");
+const thanksContainer = document.querySelector(".thank-you-container");
+
+const ratings = ratingsContainer.querySelectorAll(".icon-container");
+
 const rateMessage = document.querySelector(".selected-rating");
+
+const submitBtn = document.querySelector(".submit-btn");
 
 const ratingList = [...ratings];
 let rateChoice;
@@ -10,21 +14,17 @@ let rateChoice;
 // this function handles the click that happens on the rating.
 const handleClick = (choice, event) => {
   // go through the current rating list and remove any active styling
-  ratingList.map((rating) =>
-    rating.parentElement.classList.remove("active-rating")
-  );
+  ratingList.map((rating) => rating.classList.remove("active-rating"));
 
   // store the rating clicked on and set active styling to the number clicked
   rateChoice = event.currentTarget.dataset.rating;
-  choice.parentElement.classList.add("active-rating");
+  choice.classList.add("active-rating");
 };
 
 // go through the list of ratings and add a click event listener to the parent
 // container of the number selected
 ratingList.map((rating) => {
-  rating.parentElement.addEventListener("click", (event) =>
-    handleClick(rating, event)
-  );
+  rating.addEventListener("click", (event) => handleClick(rating, event));
 });
 
 // adds a click event listener to the submit button
@@ -33,8 +33,9 @@ submitBtn.addEventListener("click", () => {
   if (rateChoice !== undefined) {
     // set the confirmation message of the thankyou container with their rating
     rateMessage.textContent = `you selected ${rateChoice} out of 5`;
+
     // remove the prompt display and enable the thank you display
-    rateContainer.classList.remove("view");
-    thankYouContainer.classList.add("view");
+    promptContainer.classList.remove("view");
+    thanksContainer.classList.add("view");
   }
 });
